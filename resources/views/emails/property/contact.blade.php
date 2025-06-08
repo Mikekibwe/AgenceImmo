adb2c59cd9dc7223c1c8278b1a6b96071ff91e4e
Mikekibwe <kibwemike31@gmail.com>
2025-06-09 00:29:04 +0200

    Demande de contact

--- resources/views/emails/property/contact.blade.php
+++ resources/views/emails/property/contact.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # Nouvlle demande de contact
 
-Une Nouvlle demande de contact a été faite pour le bien <a href="{{ route('property.show', ['slug' => $property->getSlug(),'property'
+Une Nouvlle demande a été faite pour le bien <a href="{{ route('property.show', ['slug' => $property->getSlug(),'property'
 => $property]) }}">{{ $property->title }}</a>.
 
 -Prénom : {{ $data['firstname'] }}
